477d49b067bade95587e83420485e471024b5498
Otávio Luis Martins <otavioluism97@hotmail.com>
2020-07-15 14:54:10 -0300

    second commit 1.1

--- src/page/SignIn/index.tsx
+++ src/page/SignIn/index.tsx
@@ -26,7 +26,7 @@ const SignIn: React.FC = () => {
           </div>
 
           <div className="Whatsapp">
-            <a href="https://api.whatsapp.com/send?phone=5547991084972&text=%20Ol%C3%A1 Barbearia%20Santo%20Bigode!">Entre em contato via whatsapp!</a>
+            <a href="https://api.whatsapp.com/send?phone=5547992744015&text=%20Ol%C3%A1 Barbearia%20Santo%20Bigode!">Entre em contato via whatsapp!</a>
           </div>
 
           <div className="Facebook">
@@ -38,7 +38,7 @@ const SignIn: React.FC = () => {
           </div>
 
           <div className="Telefone">
-            <a href="tel:47999454227">Telefonar!</a>
+            <a href="tel:47992744015">Telefonar!</a>
           </div>
 
 
